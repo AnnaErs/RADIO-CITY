389e9f93542eb5bfb77884456f77f8388605a76d
AnnaErs <annaershova02@gamil.com>
2023-02-23 18:52:50 +0500

    feat: BIKDARTEM-44: [FRONT] Создать секцию "Клиентам"

---
+++ components/cards/ClientsCards/types.ts
@@ -0,0 +1,12 @@
+import {FC, PropsWithChildren} from "react";
+
+export type ClientRowCardsPropsType = {
+   src: string;
+   title: string;
+   paragraph: string;
+   isReverse: boolean;
+};
+export type ClientRowCardsType = FC<ClientRowCardsPropsType>
+
+export type ClientCardPropsType = PropsWithChildren<{}>;
+export type ClientCardType = FC<ClientCardPropsType>
--- components/sections/Clients/ClientsSection.tsx
+++ components/sections/Clients/ClientsSection.tsx
@@ -1,19 +1,41 @@
 import { memo } from "react";
 
 import Section from "@components/layout/Section";
+import Client1Image from "@public/ClientsImage1.png";
+import Client2Image from "@public/ClientsImage2.png";
+import ClientRowCards from "@components/cards/ClientsCards/ClientRowCards";
+import { title } from "process";
 
-const SECTION_TITLE = {
+const SECTION = {
     title: "Клиентам",
+    clients: [
+        {
+            src: Client1Image as any as string,
+            title: "Для частных лиц",
+            paragraph:
+                "Если вы любите активный отдых и туризм в равной степени с безопасностью, то Вам необходима временная аренда наших готовых решений для обеспечения бесперебойной связи.",
+        },
+        {
+            src: Client2Image as any as string,
+            title: "Для организаций",
+            paragraph:
+                "Если вы любите активный отдых и туризм в равной степени с безопасностью, то Вам необходима временная аренда наших готовых решений для обеспечения бесперебойной связи.",
+        },
+    ],
 };
 
-const AboutSection = () => {
+const ClientsSection = () => {
     return (
-        <div className="flex">
-            <Section title={SECTION_TITLE.title}>
-                <></>
-            </Section>
-        </div>
+        <Section title={SECTION.title}>
+            {SECTION.clients.map((client, index) => (
+                <ClientRowCards
+                    {...client}
+                    isReverse={!!(index % 2)}
+                    key={title}
+                />
+            ))}
+        </Section>
     );
 };
 
-export default memo(AboutSection);
+export default memo(ClientsSection);
