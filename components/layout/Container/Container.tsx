68ab0aa8deb14caabc39b978ef2821f429e7fbd4
AnnaErs <annaershova02@gamil.com>
2023-02-27 14:55:06 +0500

    review fix

--- components/layout/Container/Container.tsx
+++ components/layout/Container/Container.tsx
@@ -4,13 +4,8 @@ import Title from "../Title";
 
 import { ContainerType } from "./types";
 
-const Container: ContainerType = ({ title, children }) => {
-    return (
-        <div className="px-10 w-[1440px] mx-auto">
-            <Title title={title} />
-            {children}
-        </div>
-    );
+const Container: ContainerType = ({ children }) => {
+    return <div className="px-10 w-[1440px] mx-auto">{children}</div>;
 };
 
 export default memo(Container);
