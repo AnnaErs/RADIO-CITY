fad6e93010eb4a3b4843cf2dd3910db419dc7ade
AnnaErs <annaershova02@gamil.com>
2023-03-24 21:09:02 +0500

    feat: BIKDARTEM-80: [FRONTEND] Вывести клиентов

--- components/common-components/List/Item.tsx
+++ components/common-components/List/Item.tsx
@@ -4,7 +4,7 @@ import {ListType} from "./types";
 
 const Item: ListType = ({children}) => {
     return (
-        <td className="text-h4 text-start py-2 px-2 min-w-0 h-16 last:pr-0 border-b-2 last:border-transparent ">
+        <td className="text-h4 text-start py-2 px-2 min-w-[160px] h-16 last:pr-0 border-b-2 last:border-transparent last:hidden">
             {children}
         </td>
     );
