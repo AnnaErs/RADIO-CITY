bee40ab7293155f993c16cc80dc9ac819d3a5416
mimpushkarev <107047559+mimpushkarev@users.noreply.github.com>
2023-03-28 21:13:24 +0000

    review fix

--- components/cards/AboutCards/AboutCards.tsx
+++ components/cards/AboutCards/AboutCards.tsx
@@ -5,7 +5,7 @@ import {AboutCardsType} from "./types";
 
 const AboutCards: AboutCardsType = ({aboutSegments}) => {
     return (
-        <div className="flex flex-row-mx-2 flex-wrap gap-y-60 gap-x-36 justify-center items-center children:px-2">
+        <div className="flex flex-wrap gap-y-60 gap-x-36 justify-between items-center">
             {aboutSegments.map((segment) => (
                 <AboutCard aboutSegment={segment} key={segment.title} />
             ))}
